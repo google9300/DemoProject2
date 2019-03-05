980e9d00963d2ebbea8c5df9ba781247e1375ea6
Ventzeslav Petrov <vpetrotc@training.local>
2019-03-05 21:34:37 +0200

    Uploading latest work

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   getData();
   loginDisplay();
   logIn();
+  displayMain();
 });
 
 let userName = "";
@@ -44,4 +45,35 @@ function logIn() {
       swal("Wrong Credentials!", "Try Again!", "error");
     }
   });
+}
+const injectObj = `       <div class='bg-light p-4 mainText'>
+<button><img src='./images/computerIcon.png' id='computerSupport' alt=''> </button>
+<button><img src='./images/tabletIcon.png' id='tabletSupport' alt=''></button>
+<button><img src='./images/phoneIcon.png' id='phoneSupport' alt=''></button>
+</div>
+
+<form class='loginForm hidden bg-light' id='logInForm'>
+  <div class='form-group'>
+    <label for='exampleInputEmail1'>Email address</label>
+    <input type='text' class='form-control' id='userId' aria-describedby='emailHelp' placeholder='Enter email'>
+    <small id='emailHelp' class='form-text text-muted'>We'll never share your email with anyone else.</small>
+  </div>
+  <div class='form-group'>
+    <label for='exampleInputPassword1'>Password</label>
+    <input type='password' class='form-control' id='userPassword' placeholder='Password'>
+  </div>
+  <div class='form-group form-check'>
+    <input type='checkbox' class='form-check-input' id='exampleCheck1'>
+    <label class='form-check-label' for='exampleCheck1'>Check me out</label>
+  </div>
+  <button type='button' class='btn btn-primary' id='logIn'>Submit</button>
+</form> `;
+
+function displayMain() {
+$("#homeDisplay").click(function(){
+  $("#injectHere").empty();
+$("#injectHere").append(injectObj);
+
+console.log("CLICKED HOME");
+});
 }
